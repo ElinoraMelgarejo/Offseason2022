c72cdcd0a4fb5c9018acf5517e234209ddeeccb2
Elinora Melgarejo <96452829+ElinoraMelgarejo@users.noreply.github.com>
2022-09-14 10:10:17 -0400

    Create Launcher.h

---
+++ SimulationTesting/SimulationTesting/src/main/include/subsystems/Launcher.h
@@ -0,0 +1,26 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc2/command/SubsystemBase.h>
+#include <ctre/Phoenix.h>
+#include <Constants.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/shuffleboard/Shuffleboard.h>
+#include <frc/shuffleboard/ShuffleboardTab.h>
+
+class Launcher : public frc2::SubsystemBase {
+  publix:
+    Launcher();
+    void Periodic() override;
+    void RunToSpeed(void);
+    bool AtSpeed(void);
+    void Stop(void);
+  
+  private:
+    TalonFX launcher_motor{LauncherConstants::motor_controller_id};
+    std::shared_ptr<nt::NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("launcher");
+
+};
