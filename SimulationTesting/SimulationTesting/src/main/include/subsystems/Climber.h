196639603bee47a4d20a81d01dc6e70b8cd52d02
Elinora Melgarejo <96452829+ElinoraMelgarejo@users.noreply.github.com>
2022-09-14 13:20:59 -0400

    Create Climber.h

---
+++ SimulationTesting/SimulationTesting/src/main/include/subsystems/Climber.h
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc2/command/SubsystemBase.h>
+#include <ctre/Phoenix.h>
+#include <Constants.h>
+#include <frc/DigitalInput.h>
+#include <frc/Servo.h>
+
+class Climber : public frc2::SubsystemBase {
+ public:  
+  Climber();
+  void Periodic() override;
+  void Stop(void);
+  void RaiseClimber(void);
+  void LowerClimber(void);
+  int  GetLimitSwitches(void);
+  
+ private:
+  WPI_VictorSPX climber_right_motor = {ClimberConstants::right_climber_motor_id};
+  WPI_VictorSPX climber_left_motor = {ClimberConstants::left_climber_motor_id};
+
+  frc::DigitalInput top_left_limit_switch{ClimberConstants::top_left_limit_switch_port};
+  frc::DigitalInput bottom_left_limit_switch{ClimberConstants::bottom_left_limit_switch_port};
+  frc::DigitalInput top_right_limit_switch{ClimberConstants::top_right_limit_switch_port};
+  frc::DigitalInput bottom_right_limit_switch{ClimberConstants::bottom_right_limit_switch_port};
+
+  frc::Servo left_servo{ClimberConstants::left_servo_port};
+  frc::Servo right_servo{ClimberConstants::right_servo_port};
+
+};
