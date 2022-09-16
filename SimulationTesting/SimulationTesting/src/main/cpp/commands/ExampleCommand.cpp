1eb7e5cc5490f8a44aee2ca939ee45d01f4d9910
Elinora Melgarejo <96452829+ElinoraMelgarejo@users.noreply.github.com>
2022-09-16 10:23:53 -0400

    Update ExampleCommand.cpp

--- SimulationTesting/SimulationTesting/src/main/cpp/commands/ExampleCommand.cpp
+++ SimulationTesting/SimulationTesting/src/main/cpp/commands/ExampleCommand.cpp
@@ -2,7 +2,21 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-#include "commands/ExampleCommand.h"
+#include "commands/TurnToAngleLimelight.h"
 
-ExampleCommand::ExampleCommand(ExampleSubsystem* subsystem)
-    : m_subsystem{subsystem} {}
+TurnToAngleLimelight::TurnToAngleLimelight(Drive* m_drive, Limelight* m_limelight)
+    : CommandHelper(
+            frc2::PIDController(DriveConstants::limelight_PID_coefficients.kP, DriveConstants::limelight_PID_coefficients.kI, DriveConstants::limelight_PID_coefficients.kD),
+            [m_limelight] { return m_limelight->GetHorizontalOffset(); },
+            [] { return 0; },
+            [m_drive] (double output) {
+                m_drive->ArcadeDrive(0, output);
+                
+            })  {
+                    AddRequirements({m_drive, m_limelight});
+            }
+
+// Returns true when the command should end.
+bool TurnToAngleLimelight::IsFinished() {
+  return m_controller.AtSetpoint();
+}
